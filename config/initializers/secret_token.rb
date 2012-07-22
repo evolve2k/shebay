2c620f18f44bccc60838a528a8a4906501087900
Richie Khoo <evolve2k@gmail.com>
2012-07-22 23:48:35 +0930

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shebay::Application.config.secret_token = 'c7e5e25b997520d4fb98f3c9c0466c5e72a9e49d5f79b2fc96e3db9c7f406c7e798e20f84cd05cd77ff3dab2b5d240a7c5d7179a778d666d79e4340b4441ed74'
